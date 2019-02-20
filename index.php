adab4f5a8a472bfccc3980832153260b7ad25505
ZamLeu <47810828+ZamLeu@users.noreply.github.com>
2019-02-20 16:15:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTEM SALAH LAKU MURID KOLEJ VOKASIONAL SEPANG,SELANGOR</title>
+
+</head>
+<body>
+<center>
+	<h3>Log Masuk Sistem Salah Laku Murid<br>KOLEJ VOKASIONAL SEPANG,DENGKIL,SELANGOR</h3>
+<center>
+<table width="100%">
+<tr>
+<td>
+	<center>
+		<a href="login.php?aras=Pensyarah">
+		<img border="0" alt="Pensyarah" src="img/icon-lecturer_2.png" width="200" height="200"></a>
+	</center>
+</td>
+<td>
+	<center>
+		<a href="login.php?aras=Pelajar">
+		<img border="0" alt="Pelajar" src="img/icon-student_2.png" width="200" height="200"></a>
+	</center>
+</td>
+<td>
+	<center>
+		<a href="login.php?aras=Admin">
+		<img border="0" alt="Admin" src="img/icon-admin_2.png" width="200" height="200"></a>
+	</center>
+</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
